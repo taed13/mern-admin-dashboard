8e54adeb6a288dd468f67dc42584d240086e127d
taed13 <taedtech13@gmail.com>
2024-02-12 23:10:08 +0700

    building navbar with mui up to not yet create folder component

---
+++ server/index.js
@@ -0,0 +1,57 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+
+import clientRoutes from "./routes/clientRoute.js";
+import generalRoutes from "./routes/generalRoute.js";
+import managementRoutes from "./routes/managementRoute.js";
+import salesRoutes from "./routes/salesRoute.js";
+
+/** CONFIGURATION **/
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// ROUTES
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+// MONGO SET UP
+const connectDatabase = () => {
+  const mongoDbUrl = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`;
+  console.log(`Connecting to ${mongoDbUrl}`);
+  mongoose.Promise = global.Promise;
+
+  // connecting to the database
+  mongoose
+    .connect(mongoDbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Successfully connected to the database");
+    })
+    .catch((err) => {
+      console.log("Could not connect to the database. Exiting now...", err);
+      process.exit();
+    });
+};
+
+connectDatabase();
+
+const server = app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port: ${process.env.PORT}`);
+});
